9e1b32dd26e29c17a360ee8218504cd23e27f995
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2017-08-06 18:11:20 +0200

    feat: Create Book component

---
+++ src/components/Book/Book.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Book = function (props) {
+    const {book} = props;
+    const author = book.authors.join(', ');
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <img className="book-cover" src={book.imageLinks.smallThumbnail} alt={book.title}/>
+                <div className="book-shelf-changer">
+                    <select>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
+            </div>
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{author}</div>
+        </div>
+    );
+};
+
+export default Book;
+
