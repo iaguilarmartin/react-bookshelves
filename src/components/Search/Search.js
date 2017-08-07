b120b6ffe819d0a3c590b8d85131323355ef3062
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2017-08-07 13:32:50 +0200

    feat: Implement search functionallity inside Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import BooksGrid from '../BooksGrid/BooksGrid';
+import * as BooksAPI from '../../utils/BooksAPI';
 
-const Search = function (props) {
-    return (
-        <div className="search-books">
-            <div className="search-books-bar">
-                <Link to="/" className="close-search">Close</Link>
-                <div className="search-books-input-wrapper">
-                    {/*
-                     NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                     You can find these search terms here:
-                     https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class Search extends Component {
 
-                     However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                     you don't find a specific author or title. Every search is limited by search terms.
-                     */}
-                    <input type="text" placeholder="Search by title or author"/>
+    state = {
+        books: null,
+        searching: false
+    };
 
+    handleKeyPress(event) {
+        if(event.key === 'Enter') {
+            const query = event.target.value;
+
+            if (query.length === 0) {
+                this.setState({books: []});
+                return;
+            }
+
+            event.target.value = '';
+            this.setState({searching:true});
+
+            BooksAPI.search(query, 20)
+                .then(response => {
+                    let books = response.error ? [] : response;
+
+                    books = books.map(book => {
+                        const existingBook = this.props.books.find(b => b.id === book.id);
+                        book.shelf = existingBook && existingBook.shelf ? existingBook.shelf : 'none';
+                        return book;
+                    });
+
+                    this.setState({books, searching:false})
+                })
+                .catch(err => console.error('Error searching books using API service with query: ' + query, err));
+        }
+    }
+
+    render() {
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/" className="close-search">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author" onKeyPress={e => this.handleKeyPress(e)}/>
+                        {this.state.searching && (<div className="search-books-input-animation" />)}
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    {this.state.books !== null && this.state.books.length === 0 && (
+                        <div className="search-no-results">The search returned 0 results</div>
+                    )}
+
+                    <BooksGrid books={this.state.books} onBookshelfChange={this.props.onBookshelfChange}/>
                 </div>
             </div>
-            <div className="search-books-results">
-                <BooksGrid books={props.books}/>
-            </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default Search;
